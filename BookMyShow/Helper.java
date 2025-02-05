df427651da631687636408faa341d151626d6eb2
Navyamathan <navyamathan@gmail.com>
2025-02-05 11:39:58 +0530

    Add files via upload

---
+++ BookMyShow/Helper.java
@@ -0,0 +1,20 @@
+package BookMyShow;
+
+// Top Level Class
+public class Helper
+{
+    public static void main(String[] args)
+    {
+        BookMyShowActions.start(); // call start static function by its class name
+    }
+}
+
+
+
+
+
+
+
+
+
+
